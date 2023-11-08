ebd90356974dbc7ec065130a17f7cac4f9724f23
atari460 <45866241+atari460@users.noreply.github.com>
2023-11-08 17:27:37 +0700

    Bug fix in save button logic

--- mainWDP.js
+++ mainWDP.js
@@ -13,85 +13,85 @@ setInterval(function()
 	}
 }, 500);
 
-const path = decodeURI(document.location.pathname);
-try {
-//skip confirmation pop up except transition
-const realConfirm=window.confirm;
-
-if (path.split('/')[1] != 'transition'){
-	window.confirm=function(){
-		return true;
-	};
-} else {
-	window.confirm=realConfirm;
-}
 
 // Fast save code below
 // First check if we are on the node editor page, for performance reasons because we set a mutation observer
 if(currentPage.includes('node')) {
-	//skip save description
-	let saveDescNode = null;
-	let display = '';
-	const observer = new MutationObserver(function(mutations_list) {
-		mutations_list.forEach(function(mutation) {
-			mutation.addedNodes.forEach(function(added_node) {
-				if(added_node.role == 'presentation') {
-					if(added_node.innerText.indexOf('Update element description') !== -1) {
-						saveDescNode = added_node;
-						saveDescNode.style.display = display;
+	const path = decodeURI(document.location.pathname);
+	try {
+		//skip confirmation pop up except transition
+		const realConfirm=window.confirm;
+		
+		if (path.split('/')[1] != 'transition'){
+			window.confirm=function(){
+				return true;
+			};
+		} else {
+			window.confirm=realConfirm;
+		}
+		//skip save description
+		let saveDescNode = null;
+		let display = '';
+		const observer = new MutationObserver(function(mutations_list) {
+			mutations_list.forEach(function(mutation) {
+				mutation.addedNodes.forEach(function(added_node) {
+					if(added_node.role == 'presentation') {
+						if(added_node.innerText.indexOf('Update element description') !== -1) {
+							saveDescNode = added_node;
+							saveDescNode.style.display = display;
+						}
+					}
+				});
+			});
+		});
+		observer.observe(document.body, { subtree: false, childList: true });
+		
+		// Add save description button
+		
+		const lis = document.getElementsByClassName("App")[0].getElementsByTagName('li');
+		let saveButtonLi = null;
+		for (var i = 0; lis[i]; i++) {
+			if(lis[i].innerText=="Save changes") {
+				saveButtonLi = lis[i];
+			}
+		}
+		const parentUl = saveButtonLi.parentNode;
+		let newSaveDescriptionLi = document.createElement("li");
+		newSaveDescriptionLi.innerHTML = saveButtonLi.innerHTML;
+		const txt = newSaveDescriptionLi.getElementsByTagName('span')[0];
+		txt.textContent = "Save description";
+		parentUl.appendChild(newSaveDescriptionLi);
+		parentUl.appendChild(saveButtonLi);
+		
+		
+			const saveChangesButton = saveButtonLi.getElementsByTagName('button')[0];
+			const saveDescriptionButton = newSaveDescriptionLi.getElementsByTagName('button')[0];
+		
+			let saveDescriptionClicked = false;
+			saveDescriptionButton.addEventListener("click", function() {
+				saveDescriptionClicked = true;
+				saveChangesButton.click()
+				saveDescriptionClicked = false;
+			});
+		
+			saveChangesButton.addEventListener("click", function() {
+				if(saveDescNode) {
+					if(saveDescriptionClicked) {
+						display = '';
+						saveDescNode.style.display = '';
+					} else {
+						display = 'none';
+						saveDescNode.style.display = 'none';
+					}
+				} else {
+					if(saveDescriptionClicked) {
+						display = '';
+					} else {
+						display = 'none';
 					}
 				}
 			});
-		});
-	});
-	observer.observe(document.body, { subtree: false, childList: true });
-	
-	// Add save description button
-	
-	const lis = document.getElementsByClassName("App")[0].getElementsByTagName('li');
-	let saveButtonLi = null;
-	for (var i = 0; lis[i]; i++) {
-		if(lis[i].innerText=="Save changes") {
-			saveButtonLi = lis[i];
-		}
-	}
-	const parentUl = saveButtonLi.parentNode;
-	let newSaveDescriptionLi = document.createElement("li");
-	newSaveDescriptionLi.innerHTML = saveButtonLi.innerHTML;
-	const txt = newSaveDescriptionLi.getElementsByTagName('span')[0];
-	txt.textContent = "Save description";
-	parentUl.appendChild(newSaveDescriptionLi);
-	parentUl.appendChild(saveButtonLi);
-	
-	
-		const saveChangesButton = saveButtonLi.getElementsByTagName('button')[0];
-		const saveDescriptionButton = newSaveDescriptionLi.getElementsByTagName('button')[0];
-	
-		let saveDescriptionClicked = false;
-		saveDescriptionButton.addEventListener("click", function() {
-			saveDescriptionClicked = true;
-			saveChangesButton.click()
-			saveDescriptionClicked = false;
-		});
-	
-		saveChangesButton.addEventListener("click", function() {
-			if(saveDescNode) {
-				if(saveDescriptionClicked) {
-					display = '';
-					saveDescNode.style.display = '';
-				} else {
-					display = 'none';
-					saveDescNode.style.display = 'none';
-				}
-			} else {
-				if(saveDescriptionClicked) {
-					display = '';
-				} else {
-					display = 'none';
-				}
-			}
-		});
-	} catch{
+	} catch(e) {
 		console.log("ERROR")
 	}
 }
